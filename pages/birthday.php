79ef63a1793ca2758e252d0a83d52d124d01d62c
Arslankadiev Kirill <126884163+ArlanScream@users.noreply.github.com>
2023-07-02 00:27:39 +0300

    Update and rename birthdayInputWindow.php to birthday.php

---
+++ pages/birthday.php
@@ -0,0 +1,10 @@
+<section id='birthdaySection' class='modal'>
+    <div class="birthdaySendWindow">
+        <button class='modal__closeBtn' id='birthday__closeBtn'>x</button>
+        <h1 class="birthdaySendWindow__title">Введите вашу дату рождения</h1>
+        <form method='POST' class='birthday__body'>
+            <input type="date" name='birthday' name class='birthday__birthdayInput'>
+            <input type="submit" class='birthday__sendBtn' disabled value="Отправить">
+        </form>
+    </div>
+</section>
