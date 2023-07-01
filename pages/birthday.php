cf9c6dc7e59f27ccb2c15befc5f9594afa4bcbac
Arslankadiev Kirill <126884163+ArlanScream@users.noreply.github.com>
2023-07-02 00:33:57 +0300

    Update birthday.php

--- pages/birthday.php
+++ pages/birthday.php
@@ -1,7 +1,7 @@
 <section id='birthdaySection' class='modal'>
     <div class="birthdaySendWindow">
         <button class='modal__closeBtn' id='birthday__closeBtn'>x</button>
-        <h1 class="birthdaySendWindow__title">Введите вашу дату рождения</h1>
+        <h1 class="birthday__title">Введите вашу дату рождения</h1>
         <form method='POST' class='birthday__body'>
             <input type="date" name='birthday' name class='birthday__birthdayInput'>
             <input type="submit" class='birthday__sendBtn' disabled value="Отправить">
